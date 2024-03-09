d80f7f4ebabc9984fbd75880f96bc48114a59471
Enno Hermann <Eginhard@users.noreply.github.com>
2024-03-09 16:43:42 +0100

    Fix fairseq (#11)
    * fix fairseq mode
    
    * Added line to fix fairseq model issue and made code cleaner.
    
    ---------
    
    Co-authored-by: akgupta1337 <akgupta1337@gmail.com>

--- TTS/utils/manage.py
+++ TTS/utils/manage.py
@@ -260,8 +260,7 @@ class ModelManager(object):
     def _set_model_item(self, model_name):
         # fetch model info from the dict
         if "fairseq" in model_name:
-            model_type = "tts_models"
-            lang = model_name.split("/")[1]
+            model_type, lang, dataset, model = model_name.split("/")
             model_item = {
                 "model_type": "tts_models",
                 "license": "CC BY-NC 4.0",
