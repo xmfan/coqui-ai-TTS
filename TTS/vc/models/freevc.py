3a20f4725fadca3dc6efa8a597eee18afd411fb8
ChristianRomberg <distjubo@gmail.com>
2024-06-16 21:24:03 +0200

    fix(freevc): use the specified device for pretrained speaker encoder (#45)
    Fixes coqui-ai#3787

--- TTS/vc/models/freevc.py
+++ TTS/vc/models/freevc.py
@@ -382,7 +382,8 @@ class FreeVC(BaseVC):
         """Load pretrained speaker encoder model as mentioned in the paper."""
         logger.info("Loading pretrained speaker encoder model ...")
         self.enc_spk_ex = SpeakerEncoderEx(
-            "https://github.com/coqui-ai/TTS/releases/download/v0.13.0_models/speaker_encoder.pt"
+            "https://github.com/coqui-ai/TTS/releases/download/v0.13.0_models/speaker_encoder.pt",
+            device=self.device
         )
 
     def init_multispeaker(self, config: Coqpit):
